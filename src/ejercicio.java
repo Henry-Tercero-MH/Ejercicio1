ef109234896fb2b355adc84c65f86245c65650b4
Henry-Tercero-MH <hterceroh@umg.edu.gt>
2022-07-26 19:24:10 -0600

    primer ejercicio del curso

---
+++ src/ejercicio.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ejercicio {
+
+
+
+        public static void main(String[] args) {
+            String nombre;
+            Scanner entrada = new Scanner(System.in);
+            System.out.println("Digite su nombre: ");
+            nombre=entrada.nextLine();
+
+            System.out.println("Bienvenido a Progra 2 2022. "+nombre);
+
+        }
+
+    }
+
+
+
+
